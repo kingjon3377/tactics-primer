c192eebdf95baae1d1b82d884ed966607d14c6b6
Jonathan Lovelace <kingjon3377@gmail.com>
2014-04-25 19:51:03 -0400

    Clean up and convert to Unix line endings.

--- src/gameNet/GameCreator.java
+++ src/gameNet/GameCreator.java
@@ -2,43 +2,49 @@ package gameNet;
 
 import java.util.Scanner;
 
+/**
+ * An abstract class for setting up a game?
+ * 
+ * @author Clem Hasselbach (original)
+ * @author Jonathan Lovelace (cleanups, docs)
+ * 
+ */
 public abstract class GameCreator {
+	/**
+	 * @return a new initialized "game" object.
+	 */
 	public abstract GameNet_CoreGame createGame();
-	
-	public void enterGame(GameNet_UserInterface ui)
-	{
-		String playerName;
-		GamePlayer gamePlayer; 
+
+	/**
+	 * Set up a game, using the logic from the subclass.
+	 * 
+	 * @param ui
+	 *            the "user interface" object (?)
+	 */
+	public void enterGame(final GameNet_UserInterface ui) {
 		GameControl gameControl = new GameControl(this);
-		
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter your name:");
-		playerName = keyboard.next();
-	    System.out.println("Server side of game?(y/n)");
-	    String str = keyboard.next();
-	    
-	    if (str.charAt(0) == 'y')
-	    {
-	    	gameControl.startServer(); // Start a Server GameControl
-	    }
-	    else
-	    {
-	    	System.out.println("Enter ipaddress:");
-	    	String ipaddr = keyboard.next();
-	    	int port;
-	    	System.out.println("Enter port number:");
-	    	port = keyboard.nextInt();
-	    	gameControl.connect_to_server(ipaddr,port);
-	    }
-	    
-	 // Connect ourselves to the GameControl
-	    
-	    
-	    gamePlayer = new GamePlayer(playerName, gameControl, ui);
-	  
-	    ui.startUserInterface (gamePlayer);
-	    keyboard.close();
-	     
+
+		try (final Scanner keyboard = new Scanner(System.in)) {
+			System.out.print("Enter your name:\t");
+			final String playerName = keyboard.next();
+			System.out.print("Server side of game? (y/n)\t");
+			String str = keyboard.next();
+
+			if (str.charAt(0) == 'y') {
+				gameControl.startServer(); // Start a Server GameControl
+			} else {
+				System.out.print("Enter IP address:\t");
+				final String ipaddr = keyboard.next();
+				System.out.println("Enter port number:");
+				final int port = keyboard.nextInt();
+				gameControl.connect_to_server(ipaddr, port);
+			}
+
+			// Connect ourselves to the GameControl
+			final GamePlayer gamePlayer = new GamePlayer(playerName, gameControl, ui);
+
+			ui.startUserInterface(gamePlayer);
+		}
 	}
 
 }
--- src/gameNet/GameNet_CoreGame.java
+++ src/gameNet/GameNet_CoreGame.java
@@ -1,12 +1,25 @@
 package gameNet;
 
+/**
+ * The abstract class defining the interface for the game server. This really
+ * ought to be an interface, not an abstract class ...
+ * 
+ * @author Clem Hasselbach (original)
+ * @author Jonathan Lovelace (cleanups and docs)
+ * 
+ */
 public abstract class GameNet_CoreGame {
-	
-	
-	public void startGame(GameControl g)
-	{
-		
+	/**
+	 * Start the game.
+	 * @param control the parameters for the server
+	 */
+	public void startGame(@SuppressWarnings("unused") final GameControl control) {
+		// Empty default implementation.
 	}
-	
-	public abstract Object process(Object ob);
+	/**
+	 * Handle input from a client.
+	 * @param request An object encapsulating the client request
+	 * @return the response to return to the client
+	 */
+	public abstract Object process(Object request);
 }
