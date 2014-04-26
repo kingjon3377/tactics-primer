cca49050116f1679172d7a5d90dba13aba1e4c06
Jonathan Lovelace <kingjon3377@gmail.com>
2014-04-26 13:46:10 -0400

    Add Javadoc and reformat to fix warnings.

--- src/gameNet/GameNet_ConnectionBrokenInterface.java
+++ src/gameNet/GameNet_ConnectionBrokenInterface.java
@@ -1,12 +1,14 @@
 package gameNet;
 
 /**
- * Your GUI might want to implement this interface if wants to know when the 
- * socket connection is broken. Since this is optional, to register the 
- * desire for this information, call the GamePlayer method:
+ * Your GUI might want to implement this interface if wants to know when the
+ * socket connection is broken. Since this is optional, to register the desire
+ * for this information, call the GamePlayer method:
  * setGameConnectionBroken(Interface_ConnectionBroken gcb)
  */
-public interface GameNet_ConnectionBrokenInterface
-{
-    public void gameConnectionBroken();
+public interface GameNet_ConnectionBrokenInterface {
+	/**
+	 * Handle broken connection.
+	 */
+	public void gameConnectionBroken();
 }
