35d57725cf57584e13ade41e25bdbada9c8d414c
Jonathan Lovelace <kingjon3377@gmail.com>
2014-04-26 23:33:28 -0400

    Convert post-constructor setup methods to alternate constructors.

--- src/gamenet/GameCreator.java
+++ src/gamenet/GameCreator.java
@@ -25,7 +25,6 @@ public abstract class GameCreator {
 	 *            the "user interface" object (?)
 	 */
 	public void enterGame(final GameNetUserInterface ui) {
-		GameControl gameControl = new GameControl(this);
 
 		try (final Scanner keyboard = new Scanner(System.in)) {
 			System.out.print("Enter your name:\t");
@@ -34,14 +33,15 @@ public abstract class GameCreator {
 			System.out.print("Server side of game? (y/n)\t");
 			String str = keyboard.next();
 
+			final GameControl gameControl;
 			if (str.charAt(0) == 'y') {
-				gameControl.startServer(); // Start a Server GameControl
+				gameControl = new GameControl(this);
 			} else {
 				System.out.print("Enter IP address:\t");
 				final String ipaddr = keyboard.next();
 				System.out.println("Enter port number:");
 				final int port = keyboard.nextInt();
-				gameControl.connect_to_server(ipaddr, port);
+				gameControl = new GameControl(this, ipaddr, port);
 			}
 
 			// Connect ourselves to the GameControl
