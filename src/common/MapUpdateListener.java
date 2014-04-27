c141bda482b38ba84c258c016b1a4d769bbc2408
Jonathan Lovelace <1522278+kingjon3377@users.noreply.github.com>
2014-04-27 16:05:13 -0400

    Add listener interface for map updates.

---
+++ src/common/MapUpdateListener.java
@@ -0,0 +1,58 @@
+package common;
+
+import model.IPoint;
+import model.ITileFixture;
+import model.TileType;
+
+/**
+ * An interface for objects that want to be notified when a map is updated. This
+ * includes both server threads communicating with clients, so they can notify
+ * their clients, and client UIs.
+ *
+ * @author Jonathan Lovelace
+ *
+ */
+public interface MapUpdateListener {
+	/**
+	 * If the map being listened to knows unit details, those will be sent for
+	 * this player's units and only proxy fixtures for other players'.
+	 *
+	 * @return what player, if any, this listener is interested in.
+	 */
+	int getPlayer();
+
+	/**
+	 * Handle a change in terrain.
+	 *
+	 * @param point
+	 *            the location of the change
+	 * @param type
+	 *            the new tile type there
+	 */
+	void terrainChanged(IPoint point, TileType type);
+
+	/**
+	 * Handle the addition of a fixture.
+	 *
+	 * @param point
+	 *            the location of the new fixture
+	 * @param fix
+	 *            the fixture to add.
+	 */
+	void fixtureAdded(IPoint point, ITileFixture fix);
+	/**
+	 * Handle the removal of a fixture.
+	 * @param point
+	 *            the location of the fixture
+	 * @param fix
+	 *            the fixture to remove.
+	 */
+	void fixtureRemoved(IPoint point, ITileFixture fix);
+	/**
+	 * Handle the movement of a fixture from one location to another.
+	 * @param source the starting point
+	 * @param dest the destination
+	 * @param fix the fixture that is moving
+	 */
+	void fixtureMoved(IPoint source, IPoint dest, ITileFixture fix);
+}
