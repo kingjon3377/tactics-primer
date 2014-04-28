8773b397d623defe3647655d9e1ab7996d46708a
Jonathan Lovelace <1522278+kingjon3377@users.noreply.github.com>
2014-04-28 18:04:10 -0400

    Add caching of Points.

--- src/model/Point.java
+++ src/model/Point.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.eclipse.jdt.annotation.Nullable;
 
 /**
@@ -15,6 +18,10 @@ public final class Point implements IPoint {
 	 * The column.
 	 */
 	private final int column;
+	/**
+	 * The Point cache.
+	 */
+	private static final Map<Integer, Point> CACHE = new ConcurrentHashMap<>(100);
 	/**
 	 * @param theRow the row
 	 * @param theCol the column
@@ -55,5 +62,31 @@ public final class Point implements IPoint {
 		return this == obj || obj instanceof IPoint
 				&& row == ((IPoint) obj).getRow()
 				&& column == ((IPoint) obj).getColumn();
+	}
+
+	/**
+	 * Because there are going to be a <em>lot</em> of Points requested from
+	 * coordinates, we want to cache them. It doesn't matter much if race
+	 * conditons mean a few extra Points get created.
+	 *
+	 * @param row
+	 *            a row
+	 * @param col
+	 *            a column
+	 * @return a Point containing those coordinates
+	 */
+	public static Point of(final int row, final int col) {
+		// We use the same algorithm as hashCode to store them in the cache.
+		final Integer hash =
+				Integer.valueOf((row + col) * (row + col + 1) / 2 + col);
+		if (CACHE.containsKey(hash)) {
+			final Point retval = CACHE.get(hash);
+			assert retval != null;
+			return retval;
+		} else {
+			final Point retval = new Point(row, col);
+			CACHE.put(hash, retval);
+			return retval;
+		}
 	}
 }
